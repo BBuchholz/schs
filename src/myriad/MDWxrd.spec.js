93bc0ddd3599faffcae106c8c9e665ef3398852e
ENTOMDT <entomdt@gmail.com>
2022-06-26 14:18:29 -0500

    Add stubs for Wxrd and MDWxrd spec

---
+++ src/myriad/MDWxrd.spec.js
@@ -0,0 +1,15 @@
+/**
+ * @file Wxrd is the main controller for the Basecamp operations
+ * of Myriad.
+ */
+
+// TODO: change all instances of Wxrd to MDWxrd
+const { Wxrd } = require('./Wxrd');
+
+const wxrd = new Wxrd('test'); 
+
+it('should have at least one spec', () => {
+
+  expect(true).toBe(true);
+
+});
---
+++ src/myriad/Wxrd.spec.js
@@ -0,0 +1,14 @@
+/**
+ * @file Wxrd is the main controller for the Basecamp operations
+ * of Myriad.
+ */
+
+const { Wxrd } = require('./Wxrd');
+
+const wxrd = new Wxrd('test'); 
+
+it('should have at least one spec', () => {
+
+  expect(true).toBe(true);
+
+});
