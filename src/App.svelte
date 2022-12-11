2882dd98b2b5697bf16e34d1f87d21a43dea6f8d
ENTOMDT <entomdt@gmail.com>
2022-12-11 12:31:33 -0600

    Add sveltestrap and sketchy theme

--- src/App.svelte
+++ src/App.svelte
@@ -1,138 +1,20 @@
 <script>
-
-  import Modal from 'svelte-simple-modal';
-  import Notifications from 'svelte-notifications';   
-
-  import SvelteMarkdown from 'svelte-markdown';
-
-  import { 
-    modal, 
-    passPhrase,
-    headerQuote,
-    devMode,
-    currentMDWxrd,
-  } from './stores.js';
-
-
-  let currentTopic = '';
-  
-  const urlParams = 
-    new URLSearchParams(window.location.search);
-  
-  if(urlParams.has('passPhrase')){
-
-    $passPhrase = urlParams.get('passPhrase');
-    console.log('passPhrase set to: ' + $passPhrase);
-
-    //TODO: this should fetch from a function by uuid
-    // start with a mockup that just takes any value and returns
-    // a hardcoded one, but then replace with a lookup and error handling
-
-    $currentMDWxrd = `
-
-View at either of these sources (should be the same video, REPORT HERE if any errors are encountered)
-
-Source: https://tilvids.com/w/wm2tYLH84q5HxNXxxGPSxw
-Source: https://www.youtube.com/watch?v=CB1KE5dbOZo
-
-When you have finished viewing the video, POST A COMMENT HERE with your verifcation (this will be public)
-
-    `;
-    
-
-    console.log('currentMDWxrd is: ' + $currentMDWxrd);
-  }
-
-  if(urlParams.has('topic')){
-    $passPhrase = 'Topics';
-    currentTopic = urlParams.get('topic');
-  }
-
-  import MyriadNotification from './MyriadNotification.svelte';
-
-  import Paradiso from './pip/Paradiso.svelte';
-  import Alexandria from './Alexandria.svelte';
-  import DoorStep from './DoorStep.svelte';
-  import MasonryLayout from './MasonryLayout.svelte';
-  import KiplingLayout from './KiplingLayout.svelte';
-  import MyrKiLayout from './MyrKiLayout.svelte';
-  import VerifiableViewer from './VerifiableViewer.svelte';
-  import TopicBrowser from './TopicBrowser.svelte';
-  import HelpDesk from './HelpDesk.svelte';
-  import WxrdPage from './components/WxrdPage.svelte';
-  import LeMarchand from './LeMarchand.svelte';
-  import MDMDM from './md/MDMDM.svelte';
-  import WellOfWxrds from './md/WellOfWxrds.svelte';
-  import MountainsOfMadness from './md/MountainsOfMadness.svelte';
-  
-  function handleLeMarchandClick(){
-
-    $passPhrase = 'lament';
-  } 
-
-
-  function initialize(){
-
-    $devMode = false;
-
-    if($devMode){
-
-      $passPhrase = 'help';
-    }
-  }
-
-  initialize();
-
+  import { Button, Modal } from 'sveltestrap';
+  let isOpen = false;
+  const toggle = () => (isOpen = !isOpen);
 </script>
 
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/sketchy/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+</svelte:head>
 
-<Notifications item={MyriadNotification}>
-
-  <Modal show={$modal}>
-    
-    <main>
-
-      <img 
-        src="images/SCHS_logo.png" 
-        alt="Socratic Citadel Hearth Society logo" 
-        class="center" />
-
-      {#if $passPhrase === 'SFS'}
-
-        <KiplingLayout />
-
-      {:else if $passPhrase === 'Topics'}
-
-        <TopicBrowser {currentTopic} />
-
-      {:else if $passPhrase === 'verifyView' && $currentMDWxrd}
-
-        <VerifiableViewer />
-
-      {:else if $currentMDWxrd}
-
-        <SvelteMarkdown source={$currentMDWxrd.markDown} />
-
-      {:else}
-
-        <MasonryLayout />
-        
-      {/if}
-
-    </main>
-
-  </Modal>
-
-</Notifications>
-
-<style>
-
-.center {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 50%;
-}
-
-
-</style>
+<Button color="primary" on:click={toggle}>Hello World!</Button>
+<Modal body {isOpen} {toggle} header="Hello World!">
+  <p>There's a song that we're singing. Come on</p>
+  <img
+    src="https://i.ytimg.com/vi/NUJIRujygvY/hqdefault.jpg"
+    alt="Come on Get Happy"
+    class="img-fluid"
+  />
+</Modal>
