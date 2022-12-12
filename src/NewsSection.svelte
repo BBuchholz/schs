04af6125c4d83b4dc6b5af968d8a0247f96a4f02
ENTOMDT <entomdt@gmail.com>
2022-12-12 12:18:50 -0600

    Add news mock with link to live draft

--- src/NewsSection.svelte
+++ src/NewsSection.svelte
@@ -24,7 +24,7 @@
 <Container fluid>
   <Row>
     <Col>
-        Help goes here
+        Currently working on "A Short ChatGPT Blog Article", view the <a href="https://docs.google.com/document/d/e/2PACX-1vStvyPPHL_HPE7D3f6RB_m6r0gNf3RIVLq5XajPu7qtrXA3OfDVl-3Oxxoz4QN1OnZB-Rxst0Rr1oMY/pub">draft here</a>
     </Col>
   </Row>
 </Container>
